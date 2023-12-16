d1895ba9c7e6be3f9b9f768552744caff9daf8b5
mehmetgencv <mehmetgencv@gmail.com>
2023-12-16 21:27:41 +0300

    add controllers

---
+++ src/main/java/com/mehmetgenc/account/controller/AccountController.java
@@ -0,0 +1,26 @@
+package com.mehmetgenc.account.controller;
+
+import com.mehmetgenc.account.dto.AccountDto;
+import com.mehmetgenc.account.dto.CreateAccountRequest;
+import com.mehmetgenc.account.service.AccountService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/account")
+public class AccountController {
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountDto> createAccount(@Valid @RequestBody CreateAccountRequest request){
+        return ResponseEntity.ok(accountService.createAccount(request));
+    }
+}
