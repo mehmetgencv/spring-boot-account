01ae7a53beaa8f1e0a457593e85f2625381dcc49
mehmetgencv <mehmetgencv@gmail.com>
2023-12-16 21:26:49 +0300

    update models

--- src/main/java/com/mehmetgenc/account/model/Account.kt
+++ src/main/java/com/mehmetgenc/account/model/Account.kt
@@ -7,6 +7,7 @@ import java.time.LocalDateTime
 
 @Entity
 data class Account(
+
         @Id
         @GeneratedValue(generator = "UUID")
         @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
@@ -16,17 +17,19 @@ data class Account(
 
         @ManyToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
         @JoinColumn(name = "customer_id", nullable = false)
-        val customer:Customer?,
+        val customer: Customer?,
 
-        @OneToMany(mappedBy = "account", fetch = FetchType.LAZY)
+        @OneToMany(mappedBy = "account", fetch = FetchType.EAGER, cascade = [CascadeType.ALL])
         val transaction: Set<Transaction> = HashSet()
-){
+) {
+
         constructor(customer: Customer, balance: BigDecimal, creationDate: LocalDateTime) : this(
                 "",
                 customer = customer,
                 balance = balance,
                 creationDate = creationDate
         )
+
         override fun equals(other: Any?): Boolean {
                 if (this === other) return true
                 if (javaClass != other?.javaClass) return false
@@ -48,4 +51,5 @@ data class Account(
                 result = 31 * result + (customer?.hashCode() ?: 0)
                 return result
         }
+
 }
