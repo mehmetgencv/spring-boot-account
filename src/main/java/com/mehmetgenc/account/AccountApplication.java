88eca1e0e8afae25fa12da58101dbae0706de056
mehmetgencv <mehmetgencv@gmail.com>
2023-12-16 21:27:22 +0300

    add a customer for tests

--- src/main/java/com/mehmetgenc/account/AccountApplication.java
+++ src/main/java/com/mehmetgenc/account/AccountApplication.java
@@ -1,13 +1,38 @@
 package com.mehmetgenc.account;
 
+import com.mehmetgenc.account.model.Customer;
+import com.mehmetgenc.account.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Clock;
+import java.util.HashSet;
 
 @SpringBootApplication
-public class AccountApplication {
+public class AccountApplication  implements CommandLineRunner {
+
+	private final CustomerRepository customerRepository;
+
+	public AccountApplication(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args)  throws Exception{
+		Customer customer = customerRepository.save(new Customer("", "Mehmet", "Genc", new HashSet<>()));
+		Customer customer2 = customerRepository.save(new Customer("", "Meh", "Genc", new HashSet<>()));
+		System.out.println(customer);
+	}
+
+	@Bean
+	public Clock clock() {
+		return Clock.systemUTC();
 	}
 
 }
