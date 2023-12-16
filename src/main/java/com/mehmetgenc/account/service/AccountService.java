5823c2121b394342faafc6b056af9fcf8e44e587
mehmetgencv <mehmetgencv@gmail.com>
2023-12-16 21:28:24 +0300

    add clock

--- src/main/java/com/mehmetgenc/account/service/AccountService.java
+++ src/main/java/com/mehmetgenc/account/service/AccountService.java
@@ -10,38 +10,50 @@ import com.mehmetgenc.account.repository.AccountRepository;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDateTime;
 
 @Service
 public class AccountService {
+
     private final AccountRepository accountRepository;
     private final CustomerService customerService;
-    private final TransactionService transactionService;
     private final AccountDtoConverter converter;
+    private final Clock clock;
 
     public AccountService(AccountRepository accountRepository,
                           CustomerService customerService,
-                          TransactionService transactionService, AccountDtoConverter converter) {
+                          AccountDtoConverter converter, Clock clock) {
         this.accountRepository = accountRepository;
         this.customerService = customerService;
-        this.transactionService = transactionService;
         this.converter = converter;
+        this.clock = clock;
     }
 
-    public AccountDto createAccount(CreateAccountRequest createAccountRequest){
+    public AccountDto createAccount(CreateAccountRequest createAccountRequest) {
         Customer customer = customerService.findCustomerById(createAccountRequest.getCustomerId());
+
         Account account = new Account(
                 customer,
                 createAccountRequest.getInitialCredit(),
-                LocalDateTime.now()
-                );
-        if(createAccountRequest.getInitialCredit().compareTo(BigDecimal.ZERO) > 0){
-            Transaction transaction = transactionService.initiateMoney(
-                    account,
-                    createAccountRequest.getInitialCredit());
+                getLocalDateTimeNow());
+
+        if (createAccountRequest.getInitialCredit().compareTo(BigDecimal.ZERO) > 0) {
+            Transaction transaction = new Transaction(
+                    createAccountRequest.getInitialCredit(),
+                    getLocalDateTimeNow(),
+                    account);
+
             account.getTransaction().add(transaction);
         }
-
         return converter.convert(accountRepository.save(account));
+    }
+
+    private LocalDateTime getLocalDateTimeNow() {
+        Instant instant = clock.instant();
+        return LocalDateTime.ofInstant(
+                instant,
+                Clock.systemDefaultZone().getZone());
     }
 }
--- src/main/java/com/mehmetgenc/account/service/CustomerService.java
+++ src/main/java/com/mehmetgenc/account/service/CustomerService.java
@@ -1,5 +1,7 @@
 package com.mehmetgenc.account.service;
 
+import com.mehmetgenc.account.dto.CustomerDto;
+import com.mehmetgenc.account.dto.CustomerDtoConverter;
 import com.mehmetgenc.account.model.Customer;
 import com.mehmetgenc.account.repository.CustomerRepository;
 import com.mehmetgenc.account.exception.CustomerNotFoundException;
@@ -8,14 +10,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final CustomerDtoConverter converter;
 
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, CustomerDtoConverter converter) {
         this.customerRepository = customerRepository;
+        this.converter = converter;
     }
 
     protected Customer findCustomerById(String id){
         return customerRepository.findById(id).
                 orElseThrow(
                 ()-> new CustomerNotFoundException("Customer could not find by id: " + id));
+    }
+
+    public CustomerDto getCustomerById(String customerId) {
+        return converter.convertToCustomerDto(findCustomerById(customerId));
     }
 }
