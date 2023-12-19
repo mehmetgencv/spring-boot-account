5a92bbe33095a8a1b7a019a175e35382dd77db22
mehmetgencv <mehmetgencv@gmail.com>
2023-12-19 19:45:16 +0300

    add frontend

---
+++ account-api/src/main/java/com/mehmetgenc/account/configs.java
@@ -0,0 +1,25 @@
+package com.mehmetgenc.account;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class configs {
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String description,
+                                 @Value("${application-version}") String version){
+        System.out.println("sdsadasdasdasdasdasdasdasdasdas5d5asd5as5da5sd");
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Account API")
+                        .version(version)
+                        .description(description)
+                        .license(new License().name("Account API Licence")));
+    }}
